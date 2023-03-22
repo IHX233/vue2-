c6f9403341c3d9388f35c5ecc6c6a4ef40eb8343
ihx <2944177547@qq.com>
2023-03-22 23:58:01 +0800

    标签解析为ast树

--- dist/umd/vue.js
+++ dist/umd/vue.js
@@ -174,22 +174,79 @@
   var ncname = "[a-zA-Z_][\\-\\.0-9_a-zA-Z]*";
   var qnameCapture = "((?:".concat(ncname, "\\:)?").concat(ncname, ")");
   var startTagOpen = new RegExp("^<".concat(qnameCapture)); //标签开头的正则，捕获的是标签名
+  var endTag = new RegExp("^<\\/".concat(qnameCapture, "[^>]*>")); //匹配标签结尾
   var attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>']+)))?/; //匹配属性
   var startTagClose = /^\s*(\/?)>/; //匹配标签结束
-  function start(tagName, attrs) {
-    console.log(tagName, attrs);
-  }
   function parseHtml(html) {
+    function creatASTElement(tagName, attrs) {
+      return {
+        tag: tagName,
+        type: 1,
+        children: [],
+        attrs: attrs,
+        parent: null
+      };
+    }
+    //被解析的根元素
+    var root;
+    //当前在被解析的元素
+    var currentParent;
+    //用于对比标签是否符合规范，即有开始标签也有结束标签
+    var stack = [];
+    function start(tagName, attrs) {
+      var element = creatASTElement(tagName, attrs);
+      if (!root) {
+        root = element;
+      }
+      currentParent = element;
+      stack.push(element); //将生成的ast元素放到栈中
+    }
+
+    function end() {
+      var element = stack.pop(); //取出栈里最后一个，即该标签解析结束
+      currentParent = stack[stack.length - 1]; //更新当前解析的标签
+      if (currentParent) {
+        //当一个标签解析完成，就可以知道其父标签  
+        element.parent = currentParent;
+        currentParent.children.push(element);
+      }
+    }
+    function chars(text) {
+      text = text.replace(/\s/g, '');
+      if (text) {
+        currentParent.children.push({
+          type: 3,
+          text: text
+        });
+      }
+    }
     while (html) {
       var textEnd = html.indexOf('<');
       if (textEnd == 0) {
+        //是标签的开始标记
         var startTagMatch = parseStartTag();
         if (startTagMatch) {
           start(startTagMatch.tagName, startTagMatch.attrs);
+          continue;
         }
-        break;
+        var endTagMatch = html.match(endTag);
+        if (endTagMatch) {
+          advance(endTagMatch[0].length);
+          end(endTagMatch[1]);
+          continue;
+        }
+      }
+      var text = void 0;
+      if (textEnd > 0) {
+        //是文本
+        text = html.substring(0, textEnd);
+      }
+      if (text) {
+        advance(text.length);
+        chars(text);
       }
     }
+    return root;
     function advance(n) {
       //截取更新html
       html = html.substring(n);
@@ -221,7 +278,8 @@
     }
   }
   function compileToFunctions(template) {
-    parseHtml(template);
+    var ast = parseHtml(template);
+    console.log(ast);
   }
 
   function initMixin(Vue) {
--- src/compiler/index.js
+++ src/compiler/index.js
@@ -7,26 +7,75 @@ const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s
 const startTagClose = /^\s*(\/?)>/  //匹配标签结束
 const defaultTagRe = /\{\{((?:.|\r?\n)+?)\}\}/g
 
-function start(tagName,attrs){
-    console.log(tagName,attrs)
-}
-function end(tagName){
-
-}
-function chars(text){
-
-}
 function parseHtml(html){
+    function creatASTElement(tagName,attrs){
+        return {
+            tag:tagName,
+            type:1,
+            children:[],
+            attrs,
+            parent:null
+        }
+    }
+    //被解析的根元素
+    let root
+    //当前在被解析的元素
+    let currentParent
+    //用于对比标签是否符合规范，即有开始标签也有结束标签
+    let stack = []
+    function start(tagName,attrs){
+        let element = creatASTElement(tagName,attrs)
+        if(!root){
+            root = element
+        }
+        currentParent = element
+        stack.push(element)//将生成的ast元素放到栈中
+    }
+    function end(){
+        let element = stack.pop( );//取出栈里最后一个，即该标签解析结束
+        currentParent = stack[stack.length-1] //更新当前解析的标签
+        if(currentParent){//当一个标签解析完成，就可以知道其父标签  
+            element.parent = currentParent
+            currentParent.children.push(element)
+        }
+    
+    }
+    function chars(text){
+        
+        text = text.replace(/\s/g,'')
+        if(text){
+            currentParent.children.push({
+                 type:3,
+                 text
+            })
+        }
+        
+    }
     while(html){
         let textEnd = html.indexOf('<')
-        if(textEnd == 0){
+        if(textEnd == 0){//是标签的开始标记
             const startTagMatch = parseStartTag()
             if(startTagMatch){ 
                 start(startTagMatch.tagName,startTagMatch.attrs )
+                continue
             }
-            break;
+            const endTagMatch = html.match(endTag)
+            if(endTagMatch){
+                advance(endTagMatch[0].length)
+                end(endTagMatch[1])
+                continue
+            }
+        }
+        let text
+        if(textEnd>0){//是文本
+            text = html.substring(0,textEnd)
+        }
+        if(text){
+            advance(text.length)
+            chars(text)
         }
     }
+    return root 
     function advance(n){//截取更新html
         html = html.substring(n)
     }
@@ -55,4 +104,5 @@ function parseHtml(html){
 }
 export function compileToFunctions(template){
     let ast = parseHtml(template)
+    console.log(ast)
 }
